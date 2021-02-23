0b1411ba0c84c394f911a9f6b563ac9ea28b106b
Cenbomin <70682745+chenborming@users.noreply.github.com>
2021-02-23 22:04:06 +0800

    Update BBB.js

--- backup/BBB.js
+++ backup/BBB.js
@@ -1541,7 +1541,7 @@ if ($.isNode()) {
 
 
 
-var notify = $.isNode() ? require("./sendNotify") : ``;
+
 var COOKIE = $.isNode() ? require("./zhimaCOOKIE") : ``;
 var notifyttt = 1 // 0为关闭外部推送，1为12 23 点外部推送
 var notifyInterval = 2; // 0为关闭通知，1为所有通知，2为12 23 点通知  ， 3为 6 12 18 23 点通知
